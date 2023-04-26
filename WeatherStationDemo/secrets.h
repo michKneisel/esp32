14a2e2f9e0237cd7d0a4358eac46a400d8277741
Michelle Kneisel <michelle.kneisel@standardbank.co.za>
2023-04-26 16:59:58 +0200

    added ideas to shift register code

---
+++ WeatherStationDemo/secrets.h
@@ -0,0 +1,7 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "TP-LINK_75C5"
+#define WIFI_AUTH "99650988"
+
+#endif
--- Youtube_arduino/ShiftRegister/ShiftRegisterTutorial/ShiftRegisterTutorial.ino
+++ Youtube_arduino/ShiftRegister/ShiftRegisterTutorial/ShiftRegisterTutorial.ino
@@ -21,6 +21,30 @@ void SR595N_writeByte ( const Shift_Register_595N * const sr , byte val )
     uint8_t counter;
     byte shiftVal;
 
+
+https://forum.arduino.cc/t/i-have-daisy-chained-11-74hc595n-shift-registers-but-they-are-acting-strangely/683038/7
+byte dataArray[11]; // referenced as dataArray[0] to dataArray[10]
+
+// manipulate data as needed
+dataArray[0] = some value, however you get there, same for the others
+
+// send it out
+digitalWrite (latchPin, LOW);
+for (x = 0; x <11; x = x+1){
+SPI.transfer ( dataArray[x] );
+}
+digitalWrite (latchPin, HIGH); // all outputs update on this rising edge
+
+
+https://forum.arduino.cc/t/controlling-16-leds-with-two-74hc595-shift-register/696419/2
+//                XWVUTSRQPONMLKJIHGFEDCBA
+uint32_t LED1 = 0b111111111111111111111111;
+
+shiftOut(dataPin, clockPin, MSBFIRST, LED1 >> 16); // send bits X-Q
+shiftOut(dataPin, clockPin, MSBFIRST, LED1 >> 8);  // send bits P-I
+shiftOut(dataPin, clockPin, MSBFIRST, LED1);       // send bits H-A
+
+
     for ( counter = 0; counter < NUM_BITS_IN_BYTE ; counter++ )
     {
         shiftVal = ( val & 0x01 );
@@ -50,7 +74,18 @@ void setup()
 uint8_t readVal = 0;
 void loop()
 {
-    SR595N_writeByte ( &sr1, 0x3);
-    Serial.println(readVal);
-    delay(100);
+    SR595N_writeByte ( &sr1, 0b10010010);
+    delay(1000);
+    SR595N_writeByte ( &sr1, 0b11011011);
+    delay(1000);
+    SR595N_writeByte ( &sr1, 0b10110110);
+    delay(1000);
+    SR595N_writeByte ( &sr1, 0b01001001);
+    delay(1000);
+    SR595N_writeByte ( &sr1, 0b00100100);
+    delay(1000);
+    SR595N_writeByte ( &sr1, 0b11111111);
+    delay(1000);
+    SR595N_writeByte ( &sr1, 0b00000000);
+    delay(1000);
 }
